f38f2599b9522633779b62ac17558ae76f311b87
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-05-20 20:05:50 +0200

    comming soon page for heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+
   def home
   end
+
+  def war
+    @first_repo = Github.new.repos.stats.contributors params[:war][:github_username_one], params[:war][:repository_one]
+    @second_repo = Github.new.repos.stats.contributors params[:war][:github_username_two], params[:war][:repository_two]
+  end
+
 end
