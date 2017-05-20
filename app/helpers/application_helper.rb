a85db60d76ab3cd3d74833b0e8e6066a91258a35
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-05-20 20:05:50 +0200

    comming soon page for heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  require 'open-uri'
+
+  def github_api_parser(attributes)
+    json_string = open("https://api.github.com/repos/#{attributes[:username]}/#{attributes[:repo]}/commits").read
+    JSON.parse(json_string)
+  end
 end
